c9c888b1b1228a6c92f4403e1b51b88e9c74e44e
VovaDem <babujloh1993@gmail.com>
2023-11-08 05:26:24 +0300

    config error

---
+++ cmd/urlshort/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"apiserv/internal/config"
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	fmt.Println(cfg)
+	// TODO: init loger: slog
+	// TODO: init storage: sqlite
+	// TODO: init router: gochi-chi, chi-render
+	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
